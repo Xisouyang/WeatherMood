d9f6b9f62b7c74403d1d976cc226ff73d27eb752
Xi Ouyang <xisouyang@gmail.com>
2019-07-31 20:29:21 -0700

    Initial Commit

---
+++ WeatherMood/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WeatherMood
+//
+//  Created by Stephen Ouyang on 7/31/19.
+//  Copyright © 2019 Stephen Ouyang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
